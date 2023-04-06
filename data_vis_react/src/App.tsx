a76c48bdaf8c9e225ba45cd873a3c6c70f95fbe5
davidayu <dyu22@calpoly.edu>
2023-04-06 05:21:08 -0700

    Changed user list to user answer list

--- data_vis_react/src/App.tsx
+++ data_vis_react/src/App.tsx
@@ -2,21 +2,24 @@ import './App.css';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-export type User = {
-  id: number;
-  username: string;
+export type UserAnswer = {
+  text_answer: string | null;
+  slider_answer: number | null;
+  numeric_answer: number | null;
+  timestamp: string;
 };
+
 function App() {
-  const [users, setUsers] = useState<User[] | null>();
+  const [userAnswers, setUserAnswers] = useState<UserAnswer[] | null>();
 
   useEffect(() => {
-    const url = 'http://127.0.0.1:8000/user_list/';
+    const url = 'http://127.0.0.1:8000/questionnaire_user_answers/36/72/';
     axios.get(url).then((response) => {
-      setUsers(response.data);
+      setUserAnswers(response.data);
     });
   }, []);
-  return <div className="App">{users ? users.map((user) => {
-    return <p>{user.id + " " + user.username}</p>
+  return <div className="App">{userAnswers ? userAnswers.map((userAnswer) => {
+    return <p>{userAnswer.timestamp + ", " + userAnswer.text_answer + ", " + userAnswer.slider_answer + ", " + userAnswer.numeric_answer}</p>
   }) : null}</div>;
 
 }
